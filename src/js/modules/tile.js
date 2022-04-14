398b143bd1a16da782e6b0a44032de3469cbef2f
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2022-04-15 07:58:28 +0900

    update Tile class

--- src/js/modules/tile.js
+++ src/js/modules/tile.js
@@ -1,31 +1,11 @@
 export class Tile {
-  constructor(x, y, n, status = null) {
+  constructor(x, y, mark = " ") {
     this.x = x;
     this.y = y;
-    this.n = n;
-    this.status = status;
+    this.mark = mark;
   }
 
-  toString() {
-    if (this.status) {
-      return this.x + "," + this.y + "," + this.n + "," + this.status;
-    }
-    return this.x + "," + this.y + "," + this.n;
-  }
-
-  rotate(m = 1) {
-    m = (m + 4) % 4;
-
-    if (m > 1) {
-      return this.rotate().rotate(m - 1);
-    }
-
-    if (m == 1) {
-      const x = this.y;
-      const y = 3 - this.x;
-      return new Tile(x, y, this.n, this.status);
-    }
-
-    return this; // m == 0
+  isBlank() {
+    return this.mark === " ";
   }
 }
