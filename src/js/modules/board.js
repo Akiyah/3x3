26dd1cd46e15f260e49023e95989d805cd5833cf
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2022-04-16 04:04:31 +0900

    update board

---
+++ src/js/modules/board.js
@@ -0,0 +1,156 @@
+export class Board {
+  constructor(marks = this.mapPoints(() => "_")) {
+    this.marks = marks;
+  }
+
+  static create(key) {
+    const marks = key.split("\n").map(row => {
+      return row.split("");
+    });
+    return new Board(marks);
+  }
+
+  static normalKeys(board = new Board(), keys = []) {
+    const key = board.normalize().key();
+    if (keys.includes(key)) {
+      return;
+    }
+    keys.push(key);
+
+    if (board.status() === "") {
+      board.blankPoints().map(([x, y]) => {
+        const nextBoard = board.step(x, y);
+        Board.normalKeys(nextBoard, keys);
+      });
+    }
+
+    return keys;
+  }
+
+  key() {
+    return this.marks.map(row =>
+      row.map(mark => mark).join("")
+    ).join("\n");
+  }
+
+  mapPoints(callback) {
+    return [0, 1, 2].map(y => [0, 1, 2].map(x => callback(x, y)));
+  }
+
+  mark(x, y) {
+    return this.marks[y][x];
+  }
+
+  markIndex(x, y) {
+    const mark = this.mark(x, y);
+    if (mark === '_') {
+      return 0;
+    } else if (mark === 'o') {
+      return 1;
+    } else {
+      return 2;
+    }
+  }
+
+  step(x, y) {
+    const k = 9 - this.blankPoints().length;
+    const mark = ((k % 2 == 0) ? 'o' : 'x');
+
+    let marks = this.mapPoints((x0, y0) => this.mark(x0, y0));
+    marks[y][x] = mark;
+    return new Board(marks);
+  }
+
+  index() {
+    return this.mapPoints((x, y) => this.markIndex(x, y) * 3 ** (x + 3 * y)).flat().reduce((s, e) => s + e);
+  }
+
+  isBlank(x, y) {
+    return this.mark(x, y) === "_";
+  }
+
+  blankPoints() {
+    return this.mapPoints((x, y) => this.isBlank(x, y) ? [x, y] : null).flat().filter(p => p);
+  }
+
+  rotate() {
+    const marks = this.mapPoints((x, y) => this.mark(y, 2 - x));
+    return new Board(marks);
+  }
+
+  turn() {
+    const marks = this.mapPoints((x, y) => this.mark(2 - x, y));
+    return new Board(marks);
+  }
+
+  translateBoards() {
+    let board = this;
+    let boards = [];
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < 4; j++) {
+        boards.push(board);
+        board = board.rotate();
+      }
+      board = board.turn();
+    }
+    return boards;
+  }
+
+  normalize() {
+    const boards = this.translateBoards();
+    const indexes = boards.map(board => board.index());
+    const minIndex = Math.min(...indexes);
+    return boards.find(board => board.index() === minIndex);
+  }
+
+  random(n) {
+    return Math.floor(Math.random() * n);
+  }
+
+  lines() {
+    let lines = [];
+    lines = lines.concat([0, 1, 2].map(x => [0, 1, 2].map(y => [x, y]))); // |
+    lines = lines.concat([0, 1, 2].map(y => [0, 1, 2].map(x => [x, y]))); // -
+    lines = lines.concat([[0, 1, 2].map(i => [i, i])]); // \
+    lines = lines.concat([[0, 1, 2].map(i => [i, 2 - i])]); // /
+    return lines;
+  }
+
+  isWin(mark) {
+    return this.lines().some(line =>
+      line.every(([x, y]) => this.mark(x, y) == mark)
+    );
+  }
+
+  status() {
+    if (this.isWin("o")) {
+      return "o win";
+    }
+
+    if (this.isWin("x")) {
+      return "x win";
+    }
+
+    if (this.blankPoints() == 0) {
+      return "draw";
+    }
+
+    return "";
+  }
+
+  reword() {
+    if (this.isWin("o")) {
+      return 1;
+    }
+
+    if (this.isWin("x")) {
+      return -1;
+    }
+
+    if (this.blankPoints() == 0) {
+      return 0;
+    }
+
+    return null;
+  }
+}
---
+++ test/js/modules/board.test.js
@@ -0,0 +1,375 @@
+import { Board } from '../../../src/js/modules/board.js';
+
+describe('constructor', () => {
+  test('no params', () => {
+    const board = new Board();
+
+    expect(board.marks).toEqual([
+      ["_", "_", "_"],
+      ["_", "_", "_"],
+      ["_", "_", "_"]
+    ]);
+  });
+
+  test('with params', () => {
+    const board = new Board([
+      ["_", "_", "_"],
+      ["_", "_", "_"],
+      ["_", "o", "_"]
+    ]);
+
+    expect(board.marks).toEqual([
+      ["_", "_", "_"],
+      ["_", "_", "_"],
+      ["_", "o", "_"]
+    ]);
+  });
+});
+
+test('.create', () => {
+  const key =
+    "___" + "\n" +
+    "x__" + "\n" +
+    "_o_";
+  const board = Board.create(key);
+  expect(board.marks).toEqual([
+    ["_", "_", "_"],
+    ["x", "_", "_"],
+    ["_", "o", "_"]
+  ]);
+});
+
+test('.normalKeys', () => {
+  const keys = Board.normalKeys();
+
+  expect(keys.length).toBe(765);
+  expect(keys).toContain(
+    "___" + "\n" +
+    "___" + "\n" +
+    "___"
+  );
+  expect(keys).toContain(
+    "_x_" + "\n" +
+    "o__" + "\n" +
+    "___"
+  );
+  expect(keys).not.toContain(
+    "___" + "\n" +
+    "x__" + "\n" +
+    "_o_"
+  );
+});
+
+test('#key', () => {
+  let board = new Board();
+  board = board.step(1, 2);
+  board = board.step(0, 1);
+  expect(board.key()).toEqual(
+    "___" + "\n" +
+    "x__" + "\n" +
+    "_o_"
+  );
+});
+
+test('#mapPoints', () => {
+  let board = new Board();
+  board = board.step(1, 2);
+  board = board.step(0, 1);
+  const result = board.mapPoints((x, y) => [x, y]);
+  expect(result).toEqual([
+    [[0, 0], [1, 0], [2, 0]],
+    [[0, 1], [1, 1], [2, 1]],
+    [[0, 2], [1, 2], [2, 2]]
+  ]);
+});
+
+test('#mark', () => {
+  const board = new Board([
+    ["_", "_", "_"],
+    ["x", "_", "_"],
+    ["_", "o", "_"]
+  ]);
+  expect(board.mark(0, 0)).toEqual("_");
+  expect(board.mark(1, 2)).toEqual("o");
+  expect(board.mark(0, 1)).toEqual("x");
+});
+
+test('#markIndex', () => {
+  const board = new Board([
+    ["_", "_", "_"],
+    ["x", "_", "_"],
+    ["_", "o", "_"]
+  ]);
+  expect(board.markIndex(0, 0)).toBe(0);
+  expect(board.markIndex(1, 2)).toBe(1);
+  expect(board.markIndex(0, 1)).toBe(2);
+});
+
+test('#step', () => {
+  let board = new Board();
+  board = board.step(1, 2);
+  expect(board.marks).toEqual([
+    ["_", "_", "_"],
+    ["_", "_", "_"],
+    ["_", "o", "_"]
+  ]);
+
+  board = board.step(0, 1);
+  expect(board.marks).toEqual([
+    ["_", "_", "_"],
+    ["x", "_", "_"],
+    ["_", "o", "_"]
+  ]);
+});
+
+test('#index', () => {
+  const board = new Board([
+    ["_", "_", "_"],
+    ["x", "_", "_"],
+    ["_", "o", "_"]
+  ]);
+  expect(board.index()).toEqual(
+    0 * 3 ** 0 + 0 * 3 ** 1 + 0 * 3 ** 2 +
+    2 * 3 ** 3 + 0 * 3 ** 4 + 0 * 3 ** 5 +
+    0 * 3 ** 6 + 1 * 3 ** 7 + 0 * 3 ** 8
+  );
+});
+
+test('#isBlank', () => {
+  let board = new Board();
+  board = board.step(1, 2);
+  board = board.step(0, 1);
+  expect(board.isBlank(0, 0)).toBeTruthy();
+  expect(board.isBlank(1, 2)).toBeFalsy();
+  expect(board.isBlank(0, 1)).toBeFalsy();
+});
+
+test('#blankPoints', () => {
+  let board = new Board();
+  board = board.step(1, 2);
+  board = board.step(0, 1);
+  expect(board.blankPoints()).toEqual([
+    [0, 0], [1, 0], [2, 0],
+    [1, 1], [2, 1],
+    [0, 2], [2, 2]
+  ]);
+});
+
+test('#rotate', () => {
+  const board = new Board([
+    ["_", "_", "_"],
+    ["x", "_", "_"],
+    ["_", "o", "_"]
+  ]);
+  expect(board.rotate().marks).toEqual([
+    ["_", "x", "_"],
+    ["o", "_", "_"],
+    ["_", "_", "_"]
+  ]);
+});
+
+test('#turn', () => {
+  const board = new Board([
+    ["_", "_", "_"],
+    ["x", "_", "_"],
+    ["_", "o", "_"]
+  ]);
+  expect(board.turn().marks).toEqual([
+    ["_", "_", "_"],
+    ["_", "_", "x"],
+    ["_", "o", "_"]
+  ]);
+});
+
+test('#translateBoards', () => {
+  const board = new Board([
+    ["_", "_", "_"],
+    ["x", "_", "_"],
+    ["_", "o", "_"]
+  ]);
+  const boards = board.translateBoards();
+  expect(boards).toHaveLength(8);
+  expect(boards[0].marks).toEqual([
+    ["_", "_", "_"],
+    ["x", "_", "_"],
+    ["_", "o", "_"]
+  ]);
+  expect(boards[1].marks).toEqual([
+    ["_", "x", "_"],
+    ["o", "_", "_"],
+    ["_", "_", "_"]
+  ]);
+  expect(boards[2].marks).toEqual([
+    ["_", "o", "_"],
+    ["_", "_", "x"],
+    ["_", "_", "_"]
+  ]);
+  expect(boards[3].marks).toEqual([
+    ["_", "_", "_"],
+    ["_", "_", "o"],
+    ["_", "x", "_"]
+  ]);
+  expect(boards[4].marks).toEqual([
+    ["_", "_", "_"],
+    ["_", "_", "x"],
+    ["_", "o", "_"]
+  ]);
+  expect(boards[5].marks).toEqual([
+    ["_", "_", "_"],
+    ["o", "_", "_"],
+    ["_", "x", "_"]
+  ]);
+  expect(boards[6].marks).toEqual([
+    ["_", "o", "_"],
+    ["x", "_", "_"],
+    ["_", "_", "_"]
+  ]);
+  expect(boards[7].marks).toEqual([
+    ["_", "x", "_"],
+    ["_", "_", "o"],
+    ["_", "_", "_"]
+  ]);
+});
+
+test('#normalize', () => {
+  const board = new Board([
+    ["_", "_", "_"],
+    ["x", "_", "_"],
+    ["_", "o", "_"]
+  ]);
+  expect(board.normalize().marks).toEqual([
+    ["_", "x", "_"],
+    ["o", "_", "_"],
+    ["_", "_", "_"]
+  ]);
+});
+
+test('#random', () => {
+  let board = new Board();
+  let results = [];
+  for (let i = 0; i < 100; i++) {
+    results.push(board.random(10));
+  }
+  expect(Math.min(...results)).toBe(0);
+  expect(Math.max(...results)).toBe(9);
+});
+
+test('#lines', () => {
+  const board = new Board();
+  expect(board.lines()).toEqual([
+    [[0, 0], [0, 1], [0, 2]], // |
+    [[1, 0], [1, 1], [1, 2]], // |
+    [[2, 0], [2, 1], [2, 2]], // |
+    [[0, 0], [1, 0], [2, 0]], // -
+    [[0, 1], [1, 1], [2, 1]], // -
+    [[0, 2], [1, 2], [2, 2]], // -
+    [[0, 0], [1, 1], [2, 2]], // \
+    [[0, 2], [1, 1], [2, 0]]  // /
+  ]);
+});
+
+describe('#isWin', () => {
+  test('no winners', () => {
+    const board = new Board();
+    expect(board.isWin('o')).toBeFalsy();
+    expect(board.isWin('x')).toBeFalsy();
+  });
+
+  test('o win', () => {
+    const board = new Board([
+      ["_", "o", "_"],
+      ["x", "o", "x"],
+      ["_", "o", "_"]
+    ]);
+    expect(board.isWin('o')).toBeTruthy();
+    expect(board.isWin('x')).toBeFalsy();
+  });
+
+  test('x win', () => {
+    const board = new Board([
+      ["o", "_", "x"],
+      ["_", "x", "_"],
+      ["x", "o", "o"]
+    ]);
+    expect(board.isWin('o')).toBeFalsy();
+    expect(board.isWin('x')).toBeTruthy();
+  });
+
+  test('draw?', () => {
+    const board = new Board([
+      ["o", "x", "o"],
+      ["x", "o", "o"],
+      ["x", "o", "x"]
+    ]);
+    expect(board.isWin('o')).toBeFalsy();
+    expect(board.isWin('x')).toBeFalsy();
+  });
+});
+
+describe('#status', () => {
+  test('no winners', () => {
+    const board = new Board();
+    expect(board.status()).toEqual("");
+  });
+
+  test('o win', () => {
+    const board = new Board([
+      ["_", "o", "_"],
+      ["x", "o", "x"],
+      ["_", "o", "_"]
+    ]);
+    expect(board.status()).toEqual("o win");
+  });
+
+  test('x win', () => {
+    const board = new Board([
+      ["o", "_", "x"],
+      ["_", "x", "_"],
+      ["x", "o", "o"]
+    ]);
+    expect(board.status()).toEqual("x win");
+  });
+
+  test('draw', () => {
+    const board = new Board([
+      ["o", "x", "o"],
+      ["x", "o", "o"],
+      ["x", "o", "x"]
+    ]);
+    expect(board.status()).toEqual("draw");
+  });
+});
+
+describe('#reword', () => {
+  test('no winners', () => {
+    const board = new Board();
+    expect(board.reword()).toBe(null);
+  });
+
+  test('o win', () => {
+    const board = new Board([
+      ["_", "o", "_"],
+      ["x", "o", "x"],
+      ["_", "o", "_"]
+    ]);
+    expect(board.reword()).toBe(1);
+  });
+
+  test('x win', () => {
+    const board = new Board([
+      ["o", "_", "x"],
+      ["_", "x", "_"],
+      ["x", "o", "o"]
+    ]);
+    expect(board.reword()).toBe(-1);
+  });
+
+  test('draw', () => {
+    const board = new Board([
+      ["o", "x", "o"],
+      ["x", "o", "o"],
+      ["x", "o", "x"]
+    ]);
+    expect(board.reword()).toBe(0);
+  });
+});
