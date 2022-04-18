d3b0f6b5d212db6ac4d0e90e3c08ef74101fb0a5
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2022-04-19 01:26:37 +0900

    add policy methods

--- src/js/modules/quality.js
+++ src/js/modules/quality.js
@@ -4,4 +4,39 @@ export class Quality {
   constructor() {
     this.map = {};
   }
+
+  m(board) {
+    const state = board.state();
+    if (!this.map[state]) {
+      this.map[state] = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+      ];
+      board.actions().forEach(([x, y]) => {
+        this.map[state][y][x] = 0;
+      });
+    }
+    return this.map[state];
+  }
+
+  get(board, action) {
+    const [x, y] = action;
+    return this.m(board)[y][x];
+  }
+
+  set(board, action, value) {
+    const [x, y] = action;
+    this.m(board)[y][x] = value;
+  }
+
+  policy(board) {
+    const vas = board.actions().map((action) => {
+      return { value: this.get(board, action), action: action };
+    });
+    const max = Math.max(...vas.map((va) => va.value));
+    return vas.filter((va) => {
+      return va.value === max;
+    }).shift().action;
+  }
 }
--- test/js/modules/quality.test.js
+++ test/js/modules/quality.test.js
@@ -1,6 +1,59 @@
 import { Quality } from '../../../src/js/modules/quality.js';
+import { Board } from '../../../src/js/modules/board.js';
 
 test('constructor', () => {
   const quality = new Quality();
   expect(quality.map).toEqual({});
+});
+
+test('#get/#set/#m', () => {
+  const quality = new Quality();
+  const state =
+    "___" + "\n" +
+    "x__" + "\n" +
+    "_o_";
+  const board = Board.create(state);
+
+  expect(quality.m(board)).toEqual([
+    [0, 0, 0],
+    [null, 0, 0],
+    [0, null, 0]
+  ]);
+
+  expect(quality.get(board, [0, 0])).toBe(0);
+  expect(quality.get(board, [1, 0])).toBe(0);
+  expect(quality.get(board, [0, 1])).toBe(null);
+  expect(quality.get(board, [2, 1])).toBe(0);
+
+  quality.set(board, [0, 0], 0.1);
+  quality.set(board, [1, 0], 0.2);
+  quality.set(board, [2, 1], 0.3);
+
+  expect(quality.get(board, [0, 0])).toBe(0.1);
+  expect(quality.get(board, [1, 0])).toBe(0.2);
+  expect(quality.get(board, [0, 1])).toBe(null);
+  expect(quality.get(board, [2, 1])).toBe(0.3);
+
+  expect(quality.m(board)).toEqual([
+    [0.1, 0.2, 0],
+    [null, 0, 0.3],
+    [0, null, 0]
+  ]);
+});
+
+test('#policy', () => {
+  const quality = new Quality();
+  const state =
+    "___" + "\n" +
+    "x__" + "\n" +
+    "_o_";
+  const board = Board.create(state);
+
+  expect(quality.policy(board)).toEqual([0, 0]);
+
+  quality.set(board, [0, 0], 0.1);
+  quality.set(board, [1, 0], 0.2);
+  quality.set(board, [2, 1], 0.3);
+
+  expect(quality.policy(board)).toEqual([2, 1]);
 });
