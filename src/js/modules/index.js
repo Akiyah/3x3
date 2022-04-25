712c43af10bf52a6f5f09d438d22bb9801e4b1f7
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2022-04-26 00:32:17 +0900

    create env

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -8,83 +8,61 @@ function td(x, y) {
   return tdDivs[x];
 };
 
-function setMark(x, y, mark) {
-  td(x, y).innerText = mark;
-};
-
-function refreshMarks(state) {
+function refresh(state) {
   [0, 1, 2].forEach(y => {
     [0, 1, 2].forEach(x => {
-      setMark(x, y, state.mark([x, y]));
+      td(x, y).innerText = state.mark([x, y]);
     });
   });
-}
 
-function refreshResult(state) {
   const winnerSpan = document.getElementById("winner");
   winnerSpan.innerText = state.winner();
 }
 
-function lastState(states) {
-  return states[states.length - 1];
-}
-
-function refresh(states) {
-  refreshMarks(lastState(states));
-  refreshResult(lastState(states));
-}
-
-function clear(states) {
-  states.splice(0);
-  states.push(new State());
-}
-
-function click(game, states, x, y) {
-  if (lastState(states).winner()) {
-    clear(states);
-    refresh(states);
+function click(env, x, y) {
+  if (env.state.winner()) {
+    env.state = new State();
+    refresh(env.state);
     return;
   }
 
-  let state = lastState(states);
-  if (!state.enable([x, y])) {
+  if (!env.state.enable([x, y])) {
     return;
   }
 
-  state = state.step([x, y]);
-  states.push(state);
-  if (state.winner()) {
-    refresh(states);
+  env.state = env.state.step([x, y]);
+  if (env.state.winner()) {
+    refresh(env.state);
     return;
   }
 
-  const action = game.findAction(state, 0);
-  state = state.step(action);
-  states.push(state);
-  refresh(states);
+  const action = env.game.findAction(env.state, 0);
+  env.state = env.state.step(action);
+  refresh(env.state);
 };
 
 function initialize() {
-  const game = new Game();
-  const states = [];
+  const env = {
+    game: new Game(),
+    state: new State(),
+    episodes: 0
+  };
 
   [0, 1, 2].forEach(y => {
     [0, 1, 2].forEach(x => {
-      td(x, y).addEventListener("click", () => click(game, states, x, y))
+      td(x, y).addEventListener("click", () => click(env, x, y))
     });
   });
-  clear(states);
 
-  let episodes = 0;
   const timer = setInterval(() => {
     for (let i = 0; i < 100; i++) {
-      const episode = game.findEpisode(0.1);
-      game.train(episode);
-      episodes++;
+      const episode = env.game.findEpisode(0.1);
+      env.game.train(episode);
+      env.episodes++;
     }
-    document.getElementById("episodes").innerText = episodes;
-    document.getElementById("q_table_count").innerText = game.quality.count();
-    if (100 * 1000 <= episodes) {
+    document.getElementById("episodes").innerText = env.episodes;
+    document.getElementById("q_table_count").innerText = env.game.quality.count();
+    if (100 * 1000 <= env.episodes) {
       clearInterval(timer);
     }
   }, 100);
