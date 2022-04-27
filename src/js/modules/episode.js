c14e96f1e51d5876846af03effee90baef864a6b
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2022-04-28 00:22:18 +0900

    add reward slot

--- src/js/modules/episode.js
+++ src/js/modules/episode.js
@@ -3,24 +3,26 @@ export class Episode {
     this.events = [];
   }
 
-  push(state, action) {
-    this.events.push({ state: state, action: action });
+  push(state, action, reward) {
+    this.events.push({ state: state, action: action, reward: reward });
   }
 
   playerEpisode(player) {
     const l = this.events.length;
     const lastEvent = this.events[l - 1];
     const episode = new Episode();
+    const b = (player == "o" ? 0 : 1);
+    const sign = (player == "o" ? 1 : -1);
 
     this.events.forEach((event, i) => {
       if (i < l - 1) {
-        if (i % 2 == (player == "o" ? 0 : 1)) {
-          episode.push(event.state, event.action);
+        if (i % 2 == b) {
+          episode.push(event.state, event.action, event.reward * sign);
         }
       }
     });
 
-    episode.push(lastEvent.state, lastEvent.action);
+    episode.push(lastEvent.state, lastEvent.action, lastEvent.reward * sign);
     return episode;
   }
 }
--- test/js/modules/episode.test.js
+++ test/js/modules/episode.test.js
@@ -12,54 +12,57 @@ test('#push', () => {
 
   const state0 = new State();
   const action0 = new Action(1, 2);
-  episode.push(state0, action0);
+  episode.push(state0, action0, 0);
 
   expect(episode.events).toEqual([
-    { state: state0, action: action0 }
+    { state: state0, action: action0, reward: 0 }
   ]);
 
   const state1 = state0.step(action0);
   const action1 = new Action(2, 2);
-  episode.push(state1, action1);
+  episode.push(state1, action1, 1);
 
   expect(episode.events).toEqual([
-    { state: state0, action: action0 },
-    { state: state1, action: action1 }
+    { state: state0, action: action0, reward: 0 },
+    { state: state1, action: action1, reward: 1 }
   ]);
 });
 
 describe('#episodeO/#episodeX', () => {
   const episode = new Episode();
   const actions = [
-    new Action(1, 1),
-    new Action(1, 0),
     new Action(0, 0),
-    new Action(2, 1),
-    new Action(2, 2)
+    new Action(1, 0),
+    new Action(2, 0),
+    new Action(0, 1),
+    new Action(1, 1),
+    new Action(2, 1)
   ];
 
-  let state = new State();
-  const states = [state].concat(actions.map((action) => {
-    state = state.step(action);
-    return state;
-  }));
+  const rewards = [1, 2, 3, 4, 5, 6];
 
-  const l = actions.length;
-  for (let i = 0; i < l; i++) {
-    episode.push(states[i], actions[i]);
+  const states = [];
+  states[0] = new State();
+  states[1] = states[0].step(actions[0]);
+  states[2] = states[1].step(actions[1]);
+  states[3] = states[2].step(actions[2]);
+  states[4] = states[3].step(actions[3]);
+  states[5] = states[4].step(actions[4]);
+
+  for (let i = 0; i < 6; i++) {
+    episode.push(states[i], actions[i], rewards[i]);
   }
-  episode.push(states[l], null);
 
   expect(episode.playerEpisode("o").events).toEqual([
-    { state: states[0], action: actions[0] },
-    { state: states[2], action: actions[2] },
-    { state: states[4], action: actions[4] },
-    { state: states[5], action: null }
+    { state: states[0], action: actions[0], reward: rewards[0] },
+    { state: states[2], action: actions[2], reward: rewards[2] },
+    { state: states[4], action: actions[4], reward: rewards[4] },
+    { state: states[5], action: actions[5], reward: rewards[5] }
   ]);
 
   expect(episode.playerEpisode("x").events).toEqual([
-    { state: states[1], action: actions[1] },
-    { state: states[3], action: actions[3] },
-    { state: states[5], action: null }
+    { state: states[1], action: actions[1], reward: -rewards[1] },
+    { state: states[3], action: actions[3], reward: -rewards[3] },
+    { state: states[5], action: actions[5], reward: -rewards[5] }
   ]);
 });
