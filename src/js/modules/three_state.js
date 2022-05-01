24be89701a65b459390a7e678f6cf79adbda97df
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2022-05-02 01:45:00 +0900

    create ThreeState

---
+++ src/js/modules/three_state.js
@@ -0,0 +1,43 @@
+import { State } from "./state.js";
+
+export class ThreeState extends State {
+  constructor(marks, count, orders) {
+    super(marks, count);
+    this.orders = orders || this.mapPoints(() => 0);
+  }
+
+  toString() {
+    return super.toString() + "\n" +
+    this.orders.map(row =>
+      row.join("")
+    ).join("\n");
+  }
+
+  step(action) {
+    const state = super.step(action);
+    const x = action.x;
+    const y = action.y;
+
+    const orders = this.mapPoints((action0) => {
+      let order = this.orders[action0.y][action0.x];
+      if (order != 0) {
+        order++;
+      }
+      if (6 < order) {
+        order = 0;
+      }
+      if (action0.x == x && action0.y == y) {
+        order = 1;
+      }
+      return order;
+    });
+    const marks = state.mapPoints((action0) => {
+      if (orders[action0.y][action0.x] == 0) {
+        return " ";
+      }
+      return state.mark(action0);
+    });
+
+    return new ThreeState(marks, state.count, orders);
+  }
+}
---
+++ test/js/modules/three_state.test.js
@@ -0,0 +1,120 @@
+import { Action } from '../../../src/js/modules/action.js';
+import { ThreeState } from '../../../src/js/modules/three_state.js';
+
+describe('constructor', () => {
+  test('no params', () => {
+    const state = new ThreeState();
+
+    expect(state.count).toBe(0);
+    expect(state.marks).toEqual([
+      [" ", " ", " "],
+      [" ", " ", " "],
+      [" ", " ", " "]
+    ]);
+    expect(state.orders).toEqual([
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ]);
+  });
+
+  test('with params', () => {
+    const state = new ThreeState([
+      [" ", " ", " "],
+      ["x", " ", " "],
+      [" ", "o", " "]
+    ], 1, [
+      [0, 0, 0],
+      [1, 0, 0],
+      [0, 2, 0]
+    ]);
+
+    expect(state.count).toBe(1);
+    expect(state.marks).toEqual([
+      [" ", " ", " "],
+      ["x", " ", " "],
+      [" ", "o", " "]
+    ]);
+    expect(state.orders).toEqual([
+      [0, 0, 0],
+      [1, 0, 0],
+      [0, 2, 0]
+    ]);
+  });
+});
+
+test('#toString', () => {
+  const state = new ThreeState([
+    [" ", " ", " "],
+    ["x", " ", " "],
+    [" ", "o", " "]
+  ], 1, [
+    [0, 0, 0],
+    [1, 0, 0],
+    [0, 2, 0]
+  ]);
+
+  expect(state.toString()).toEqual(
+    "   " + "\n" +
+    "x  " + "\n" +
+    " o " + "\n" +
+    "000" + "\n" +
+    "100" + "\n" +
+    "020"
+  );
+});
+
+test('#step', () => {
+  let state = new ThreeState();
+
+  const actions = [
+    new Action(0, 0),
+    new Action(2, 2),
+    new Action(2, 1),
+    new Action(0, 1),
+    new Action(1, 2),
+    new Action(1, 0)
+  ];
+
+  actions.forEach((action) => {
+    state = state.step(action);
+  });
+
+  expect(state.count).toBe(6);
+  expect(state.marks).toEqual([
+    ["o", "x", " "],
+    ["x", " ", "o"],
+    [" ", "o", "x"]
+  ]);
+  expect(state.orders).toEqual([
+    [6, 1, 0],
+    [3, 0, 4],
+    [0, 2, 5]
+  ]);
+
+  state = state.step(new Action(2, 0));
+  expect(state.count).toBe(7);
+  expect(state.marks).toEqual([
+    [" ", "x", "o"],
+    ["x", " ", "o"],
+    [" ", "o", "x"]
+  ]);
+  expect(state.orders).toEqual([
+    [0, 2, 1],
+    [4, 0, 5],
+    [0, 3, 6]
+  ]);
+
+  state = state.step(new Action(0, 2));
+  expect(state.count).toBe(8);
+  expect(state.marks).toEqual([
+    [" ", "x", "o"],
+    ["x", " ", "o"],
+    ["x", "o", " "]
+  ]);
+  expect(state.orders).toEqual([
+    [0, 3, 2],
+    [5, 0, 6],
+    [1, 4, 0]
+  ]);
+});
