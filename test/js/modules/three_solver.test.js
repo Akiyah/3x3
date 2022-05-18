00e2eab23e913d5df7429308aba120b28a052ec1
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2022-05-19 02:17:47 +0900

    refactoring

--- test/js/modules/three_solver.test.js
+++ test/js/modules/three_solver.test.js
@@ -1,38 +1,47 @@
 import { ThreeSolver } from '../../../src/js/modules/three_solver.js';
 
-test('#init', () => {
+describe('#init', () => {
   const solver = new ThreeSolver();
   const results = solver.init();
 
-  const keys = Object.keys(results);
-  const key = keys[keys.length - 1];
+  test('size', () => {
+    const keys = Object.keys(results);
+    expect(keys.length).toBe(128170);
+  });
 
-  expect(keys.length).toBe(128170);
-  expect(key).toBe(
-    "   " + "\n" +
-    "xox" + "\n" +
-    "oxo" + "\n" +
-    "000" + "\n" +
-    "654" + "\n" +
-    "321"
-  );
-  expect(results[key]).toBe(0);
+  test('last', () => {
+    const keys = Object.keys(results);
+    const key = keys[keys.length - 1];
+    expect(key).toBe(
+      "   " + "\n" +
+      "xox" + "\n" +
+      "oxo" + "\n" +
+      "000" + "\n" +
+      "654" + "\n" +
+      "321"
+    );
+    expect(results[key]).toBe(0);
+  });
 
-  const key2 =
-    "x  " + "\n" +
-    " x " + "\n" +
-    "ooo" + "\n" +
-    "500" + "\n" +
-    "030" + "\n" +
-    "642"
-  expect(results[key2]).toBe(1);
+  test('o win', () => {
+    const key =
+      "x  " + "\n" +
+      " x " + "\n" +
+      "ooo" + "\n" +
+      "500" + "\n" +
+      "030" + "\n" +
+      "642"
+    expect(results[key]).toBe(1);
+  });
 
-  const key3 =
-    "o  " + "\n" +
-    " oo" + "\n" +
-    "xxx" + "\n" +
-    "500" + "\n" +
-    "031" + "\n" +
-    "642"
-  expect(results[key3]).toBe(-1);
+  test('x win', () => {
+    const key =
+      "o  " + "\n" +
+      " oo" + "\n" +
+      "xxx" + "\n" +
+      "500" + "\n" +
+      "031" + "\n" +
+      "642"
+    expect(results[key]).toBe(-1);
+  });
 });
