bc478c8bc14fd29d833c510469d41f2cc7d2f6ff
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2022-05-21 12:36:55 +0900

    create update

--- src/js/modules/three_solver.js
+++ src/js/modules/three_solver.js
@@ -14,6 +14,50 @@ export class ThreeSolver {
     return data;
   }
 
+  update(data) {
+    const newData = {};
+
+    this.each((state) => {
+      const key = state.toString();
+      if (data[key] != 0) {
+        newData[key] = data[key];
+        return;
+      }
+
+      const values = state.actions().map((action) => {
+        const key = state.step(action).toString();
+        return data[key];
+      });
+
+      const valuesPlus = values.filter((v) => { return 0 < v; });
+      const valuesMinus = values.filter((v) => { return v < 0; });
+
+      if (state.nextPlayerMark() == 'o') {
+        if (0 < valuesPlus.length) {
+          newData[key] = Math.min(...valuesPlus) + 1;
+          return;
+        }
+        if (valuesMinus.length == values.length) {
+          newData[key] = Math.max(...valuesMinus) - 1;
+          return;
+        }
+        newData[key] = 0;
+      } else {
+        if (0 < valuesMinus.length) {
+          newData[key] = Math.max(...valuesMinus) - 1;
+          return;
+        }
+        if (valuesPlus.length == values.length) {
+          newData[key] = Math.min(...valuesPlus) + 1;
+          return;
+        }
+        newData[key] = 0;
+      }
+    });
+
+    return newData;
+  }
+
   each(callback) {
     const state = new ThreeState();
     this.eachOne(state, 0, callback);
--- test/js/modules/three_solver.test.js
+++ test/js/modules/three_solver.test.js
@@ -2,15 +2,15 @@ import { ThreeSolver } from '../../../src/js/modules/three_solver.js';
 
 describe('#init', () => {
   const solver = new ThreeSolver();
-  const results = solver.init();
+  const data = solver.init();
 
   test('size', () => {
-    const keys = Object.keys(results);
+    const keys = Object.keys(data);
     expect(keys.length).toBe(128170);
   });
 
   test('last', () => {
-    const keys = Object.keys(results);
+    const keys = Object.keys(data);
     const key = keys[keys.length - 1];
     expect(key).toBe(
       "   " + "\n" +
@@ -20,7 +20,7 @@ describe('#init', () => {
       "654" + "\n" +
       "321"
     );
-    expect(results[key]).toBe(0);
+    expect(data[key]).toBe(0);
   });
 
   test('o win', () => {
@@ -31,7 +31,7 @@ describe('#init', () => {
       "500" + "\n" +
       "030" + "\n" +
       "642"
-    expect(results[key]).toBe(1);
+    expect(data[key]).toBe(1);
   });
 
   test('x win', () => {
@@ -42,6 +42,34 @@ describe('#init', () => {
       "500" + "\n" +
       "031" + "\n" +
       "642"
-    expect(results[key]).toBe(-1);
+    expect(data[key]).toBe(-1);
+  });
+});
+
+describe('#update', () => {
+  const solver = new ThreeSolver();
+  const data0 = solver.init();
+  const data1 = solver.update(data0);
+
+  test('next o win', () => {
+    const key =
+      "x  " + "\n" +
+      " x " + "\n" +
+      " oo" + "\n" +
+      "600" + "\n" +
+      "040" + "\n" +
+      "053"
+    expect(data1[key]).toBe(2);
+  });
+
+  test('next x win', () => {
+    const key =
+      "o  " + "\n" +
+      " oo" + "\n" +
+      " xx" + "\n" +
+      "600" + "\n" +
+      "042" + "\n" +
+      "053"
+    expect(data1[key]).toBe(-2);
   });
 });
